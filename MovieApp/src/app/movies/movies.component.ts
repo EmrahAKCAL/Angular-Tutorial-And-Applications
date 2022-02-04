4c2d55c41cbb67e954e4cc7a47c6aaf23423339d
EmrahAKCAL <emrahakcl02@gmail.com>
2022-02-05 00:35:50 +0200

    liste elemlarını *ngFor ile ekleme

--- MovieApp/src/app/movies/movies.component.ts
+++ MovieApp/src/app/movies/movies.component.ts
@@ -1,21 +1,15 @@
 import { Component } from "@angular/core";
-import { Movie } from "../movie";
-
+import { Movies } from "../movie.datasource";
 
 @Component({
     selector: 'movies', //<movies> </movies> şeklinde çağrılır. class veya id şeklinde de tanımlanılabilir. css mantığı
     templateUrl: './movies.component.html', //içerik olarak çıkacak olan 
+    styleUrls: ['./movies.component.css']
   
 })
 export class MoviesComponent {
     title= 'Movie List';
-    getTitle(){
-        return this.title;
-    }
-    movie: Movie ={
-        id:1,
-        name: 'Movie Name',
-    }
+    movies=Movies;
 }
 
 //Oluşturmuş olduğumuz component app.module.ts içerisine import edilir.
