e3ec2d6b148d12d20d52900a3f1d9f2aa4592c05
EmrahAKCAL <emrahakcl02@gmail.com>
2022-02-12 14:11:42 +0200

    6-Listeye yeni eleman ekleme, silme, güncelleme

--- Directives/src/app/directives.component.ts
+++ Directives/src/app/directives.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Product } from "./product.model";
 import { ProductRepository } from "./repostory.model";
 
 
@@ -12,4 +13,14 @@ export class DirectivesComponent{
     //ts çalışmaların olacağı kısım 
     model: ProductRepository= new ProductRepository();
     productName: string= this.model.getProductsById(1).name;
+
+    addProduct(){
+        this.model.addProduct(new Product(6, 'Samsung S10', 'iyi telefon', '5.jpg', 6000)); // addProduct metodu çağrıldığında repository içerisine yeni bir eleman gönderilecek. Buradaki veri  repository.model içerisindeki addProduct(product: Product) metoduna gönderilir.  addProduct(product: Product) ise gelen bu veriyi  this.products.push(product); ile products içerisine ekleyecek. products ise bir Product dizisidir. 
+    }
+    deleteProduct(p: Product){
+        this.model.deleteProduct(p);
+    }
+    updateProduct(prd: Product){
+        prd.name='Update';
+    }
 }
