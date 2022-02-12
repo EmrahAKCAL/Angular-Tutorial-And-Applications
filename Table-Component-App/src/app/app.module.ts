836421f0df12fe39401f160a67c6df845c64ad87
EmrahAKCAL <emrahakcl02@gmail.com>
2022-02-12 20:33:07 +0200

    1-ürünlerin listelenmesi

--- Table-Component-App/src/app/app.module.ts
+++ Table-Component-App/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AdminProductsComponent } from './admin-products/admin-products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminProductsComponent
   ],
   imports: [
     BrowserModule
---
+++ Table-Component-App/src/app/repository.model.ts
@@ -0,0 +1,23 @@
+//liste üzerinde işlem yapacak olan metotları barındıran 
+
+//Bu sayfaya çağrılan yapılar
+import { SimpleDataSource } from "./datasource.model"; 
+import { Product } from "./product.model";
+
+export class ProductRepository{
+    private dataSource: SimpleDataSource; //datasource.model deki SimpleDataSoruce sınıfı çağrıldı ve dataSource aktarılarak private yapıldı.
+    private products: Product []; //product.model deki Product sınıfı çağrılarak products içerisine aktarıp private yapıldı
+    constructor(){ //dışardan gelen değişkenleri barındıran kalıp
+        this.dataSource = new SimpleDataSource();
+        this.products= new Array<Product>();
+        this.dataSource.getProducts().forEach(product=>this.products.push(product)); //getProducts metotundan gelen her bir elemanı foreach ile üzerinden gezer ve her gelen bir product elemanı products üzerine ekler.
+    }
+    getProductsById(id: number): Product{
+        return this.products.find(p=> p.id==id); //dışardan gelen bir id değeri liste içerisindeki her bir elemanın id değerini kontrol edecek ve eşleşme söz konusu ise Product geri döndürecek
+    }
+
+    getProducts(): Product[]{ //repository üzerinde tanımlanılmış olan products ları alıp html üzerine taşıyoruz.
+        return this.products;
+    }
+}
+
