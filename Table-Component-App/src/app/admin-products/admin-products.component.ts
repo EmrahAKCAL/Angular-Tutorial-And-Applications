dc4be47d602b233cf2fa40a0e0f4107949451761
EmrahAKCAL <emrahakcl02@gmail.com>
2022-02-13 12:42:00 +0200

    5-edit butona tıklayarak ürün detaylarnı görtermek

--- Table-Component-App/src/app/admin-products/admin-products.component.ts
+++ Table-Component-App/src/app/admin-products/admin-products.component.ts
@@ -10,13 +10,16 @@ import { ProductRepository } from '../repository.model';
 export class AdminProductsComponent implements OnInit {
   products;
   model: ProductRepository;
-  selecetedProduct: string;
+  selecetedProduct: Product;
   constructor() {
     this.model= new ProductRepository();
     this.products=this.model.getProducts();
    }
    getSelected(product:  Product):boolean{
-     return product.name==this.selecetedProduct; //selectedProduct üzerinden gelen değer ile product name değerine eşit ise true değeri döndürür
+     return product==this.selecetedProduct; //selectedProduct üzerinden gelen değer ile product name değerine eşit ise true değeri döndürür
+   }
+   editProduct(product: Product){ 
+     this.selecetedProduct=product;
    }
   ngOnInit() {
   }
