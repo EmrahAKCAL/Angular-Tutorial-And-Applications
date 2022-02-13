f56e3579d17e80cb5cbcadf52c06bf0e173fb071
EmrahAKCAL <emrahakcl02@gmail.com>
2022-02-13 11:03:17 +0200

    3-input-ile-ürün-seçme

--- Table-Component-App/src/app/admin-products/admin-products.component.ts
+++ Table-Component-App/src/app/admin-products/admin-products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model';
 import { ProductRepository } from '../repository.model';
 
 @Component({
@@ -9,9 +10,13 @@ import { ProductRepository } from '../repository.model';
 export class AdminProductsComponent implements OnInit {
   products;
   model: ProductRepository;
+  selecetedProduct: string;
   constructor() {
     this.model= new ProductRepository();
     this.products=this.model.getProducts();
+   }
+   getSelected(product:  Product):boolean{
+     return product.name==this.selecetedProduct; //selectedProduct üzerinden gelen değer ile product name değerine eşit ise true değeri döndürür
    }
   ngOnInit() {
   }
