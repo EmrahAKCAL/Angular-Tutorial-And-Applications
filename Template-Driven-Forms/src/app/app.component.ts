6bb881dc3c9c458dc83b81d5ed492c64b880dd67
EmrahAKCAL <emrahakcl02@gmail.com>
2022-02-16 17:06:42 +0200

    2-form oluşturuldu

--- Template-Driven-Forms/src/app/app.component.ts
+++ Template-Driven-Forms/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
+import { ProductRepository } from './responsive.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Template-driven-forms';
+  model: ProductRepository= new ProductRepository();
+  newProduct: Product= new Product();
+
+
+  get jsonProduct(){ //product objesini json formatına dönüştüren metot
+    return JSON.stringify(this.newProduct);
+  }
+
+  addProduct(prd: Product){ //kayıt ekleme metotu
+    console.log('New Product: '+this.jsonProduct);
+    
+  }
 }
