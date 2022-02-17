337437251ba723b46a5f1207e415a6c8c0fc67b7
EmrahAKCAL <emrahakcl02@gmail.com>
2022-02-17 16:10:50 +0200

    9-submit olayında özet mesaj gösterilmesi

--- Template-Driven-Forms/src/app/app.component.ts
+++ Template-Driven-Forms/src/app/app.component.ts
@@ -31,9 +31,16 @@ export class AppComponent {
       this.formSubmitted=false;
     }
   }
-
-  getValidationErrors(state: any){
-    let ctrlName: string= state.name;
+  getFormValidationErrors(form: NgForm): string[]{
+    let messages: string[]= [];
+    Object.keys(form.controls).forEach(k=> { //gelen form objesini diziye çevirir.
+      this.getValidationErrors(form.controls[k], k).forEach(message=> messages.push(message));
+      
+    })
+    return messages;
+  }
+  getValidationErrors(state: any, key: string){
+    let ctrlName: string= state.name || key;
     let messages: string[]= []; //messages değişkeni string tipinde bir dizidir ve ilk başta içi boştur.
     if(state.errors){
       for(let errorName in state.errors){
