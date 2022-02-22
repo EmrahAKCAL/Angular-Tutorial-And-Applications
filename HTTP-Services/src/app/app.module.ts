2d26e264c24228242bec514adca9f57c2f2874d0
EmrahAKCAL <emrahakcl02@gmail.com>
2022-02-22 15:12:56 +0200

    1-Http get request kullanımı

--- HTTP-Services/src/app/app.module.ts
+++ HTTP-Services/src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {HttpClientModule} from '@angular/common/http'
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
