e50938ef4e33dc6cc826de1640ccf55185aa9dcf
EmrahAKCAL <emrahakcl02@gmail.com>
2022-02-23 13:02:30 +0200

    6-Service Hata Denetimi

--- HTTP-Services/src/app/services/post.service.ts
+++ HTTP-Services/src/app/services/post.service.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { throwError } from 'rxjs';
+import {catchError, retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +12,11 @@ export class PostService {
   constructor(private http: HttpClient) { }
 
   getPosts(){
-    return this.http.get(this.url);
+    return this.http.get(this.url).pipe(
+      retry(3), //üç kere talep gönderir
+      catchError(this.handleError)
+    )
   }
-
   createPost(post){
     return this.http.post(this.url, JSON.stringify(post));
   }
@@ -21,6 +25,20 @@ export class PostService {
     return  this.http.put(this.url+'/'+post.id, JSON.stringify(post));
   }
   deletePost(post){
-    return this.http.delete(this.url+'/'+post.id);
+    return this.http.delete(this.url+'/'+post.id).pipe(
+      retry(3), //üç kere talep gönderir
+      catchError(this.handleError)
+    );
+  }
+  
+  private handleError(error: HttpErrorResponse){
+    if(error.error instanceof ErrorEvent){
+      //client error
+      console.log(`Client error: `+error.error.message);
+    }else{
+      //backend error
+      console.log(`Backend error:${error.status} error: ${error.error}`);
+    }
+    return throwError(`Bilinmeyen hata oluştu.`)
   }
 }
