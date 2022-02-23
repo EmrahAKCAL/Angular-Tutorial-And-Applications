3a9d976e8c90209a7b791392deed76de25735a2c
EmrahAKCAL <emrahakcl02@gmail.com>
2022-02-23 11:30:32 +0200

    5-Service ile metotların kullanımı

---
+++ HTTP-Services/src/app/services/post.service.ts
@@ -0,0 +1,26 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PostService {
+
+  private url = 'https://jsonplaceholder.typicode.com/posts';
+  constructor(private http: HttpClient) { }
+
+  getPosts(){
+    return this.http.get(this.url);
+  }
+
+  createPost(post){
+    return this.http.post(this.url, JSON.stringify(post));
+  }
+
+  updatePost(post){
+    return  this.http.put(this.url+'/'+post.id, JSON.stringify(post));
+  }
+  deletePost(post){
+    return this.http.delete(this.url+'/'+post.id);
+  }
+}
