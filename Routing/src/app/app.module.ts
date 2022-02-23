1de63aac6dc0b50b5ae0b125a7d5c11eaafae965
EmrahAKCAL <emrahakcl02@gmail.com>
2022-02-23 15:16:17 +0200

    2-Route şemasını oluşturmak

--- Routing/src/app/app.module.ts
+++ Routing/src/app/app.module.ts
@@ -6,6 +6,16 @@ import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { UsersComponent } from './users/users.component';
 import { CategoriesComponent } from './categories/categories.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NotfoundComponent } from './notfound/notfound.component';
+
+const appRoutes: Routes=[
+  { path: '',component: HomeComponent }, //kullanıcı herhangi bir url girmezse(localhost:4200)
+  { path: 'home',component: HomeComponent },
+  { path: 'products',component: ProductsComponent },
+  { path: 'users',component: UsersComponent },
+  { path: '**',component: NotfoundComponent } //kullanıcı bunlar haricinde bir url girdiğinde
+];
 
 @NgModule({
   declarations: [
@@ -13,10 +23,12 @@ import { CategoriesComponent } from './categories/categories.component';
     HomeComponent,
     ProductsComponent,
     UsersComponent,
-    CategoriesComponent
+    CategoriesComponent,
+    NotfoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
