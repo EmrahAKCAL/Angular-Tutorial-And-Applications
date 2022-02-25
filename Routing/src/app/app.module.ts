81b0160b7b1e2e0d5a668d77c20c12341eb12d60
EmrahAKCAL <emrahakcl02@gmail.com>
2022-02-25 15:25:50 +0200

    8-routes-children ile iç içe route ekleme

--- Routing/src/app/app.module.ts
+++ Routing/src/app/app.module.ts
@@ -9,13 +9,19 @@ import { CategoriesComponent } from './categories/categories.component';
 import { RouterModule, Routes } from '@angular/router';
 import { NotfoundComponent } from './notfound/notfound.component';
 import { ProductComponent } from './products/product/product.component';
+import { UserComponent } from './users/user/user.component';
+import { EditProductComponent } from './products/edit-product/edit-product.component';
 
 const appRoutes: Routes=[
   { path: '',component: HomeComponent }, //kullanıcı herhangi bir url girmezse(localhost:4200)
   { path: 'home',component: HomeComponent },
-  { path: 'products',component: ProductsComponent },
-  { path: 'products/:id',component: ProductComponent },
-  { path: 'users',component: UsersComponent },
+  { path: 'products',component: ProductsComponent, children:[
+    { path: ':id',component: ProductComponent },
+    { path: ':id/edit',component: EditProductComponent },
+  ]},
+  { path: 'users',component: UsersComponent, children: [
+    {path: ':name', component: UserComponent}
+  ]},
   { path: '**',component: NotfoundComponent } //kullanıcı bunlar haricinde bir url girdiğinde
 ];
 
@@ -29,7 +35,9 @@ const appRoutes: Routes=[
     UsersComponent,
     CategoriesComponent,
     NotfoundComponent,
-    ProductComponent
+    ProductComponent,
+    UserComponent,
+    EditProductComponent
   ],
   imports: [
     BrowserModule,
