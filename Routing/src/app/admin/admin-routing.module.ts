f7e316e24752ed0037a3ad3b68b5cd98699f27f1
EmrahAKCAL <emrahakcl02@gmail.com>
2022-02-27 10:19:36 +0200

    11-Guard:CanActivate-kullanici-paneli-yetkilendirm

--- Routing/src/app/admin/admin-routing.module.ts
+++ Routing/src/app/admin/admin-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AuthGuard } from '../auth-guard.service';
 import { AdminCategoriesComponent } from './admin-categories/admin-categories.component';
 import { AdminProductsComponent } from './admin-products/admin-products.component';
 import { AdminComponent } from './admin/admin.component';
@@ -7,7 +8,9 @@ import { AdminComponent } from './admin/admin.component';
 
 const routes: Routes = [ //linkleri buraya ekleyebiliriz.
 
-  {path: 'admin', component: AdminComponent, children: [
+  {path: 'admin', 
+  canActivate: [AuthGuard], //Bu linki her ziyaret eden bu panele erişilmemesi gerekir. Yani bu admin paneli yetkilendirilmesi gerekir. Bu kontrolü yapan CanActivate dir. Dizi olmasının nedeni birden fazla guard ı ekleyebiliriz.
+  component: AdminComponent, children: [ 
     {path: 'products', component: AdminProductsComponent },
     {path: 'categories', component: AdminCategoriesComponent}
   ]}
---
+++ Routing/src/app/auth.service.ts
@@ -0,0 +1,19 @@
+export class AuthService {
+    loggedIn= false; //kullanıcı varsayılan olarak alacak değer
+
+    isAuthenticated(){
+        const promise= new Promise((resolve, reject)=>{ //eğer başarılı bir sonuç dönüyorsa resolve içerisine, eğer bir hata sonucun döndürüyorsa reject içerisine geri gönderir.
+            setTimeout(()=>{
+                resolve(this.loggedIn); //bir hata yoksa geri loggedIn i gönderecek
+            }, 700);
+
+        });
+        return promise; //kullanıcı o anki durumunu kontrol edecek
+    }
+    login(){
+        this.loggedIn= true; //Eğer kullanıcı login() metotunu çağırırsa gelecek olan değer
+    }
+    logout(){
+        this.loggedIn= false; //Eğer kullanıcı logout() metotunu çağırırsa gelecek olan değer
+    }
+}
